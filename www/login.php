e40e489cee66cf08626974e8cc2f4770471fef69
Nu Huyen Trang Pham <nu-huyen-trang.pham@etu.utc.fr>
2019-11-16 20:51:39 +0100

    update

--- www/login.php
+++ www/login.php
@@ -7,7 +7,7 @@
 	<title>Login</title>
 </head>
 <body>
-	<?php  
+	<?php  		
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
@@ -27,17 +27,29 @@
 		$dn = $_POST["dn"];
 
 		// Requête insert into table 
-		$sql = "SELECT * FROM Client WHERE Nom='".$name."' AND Prenom='".$surname."' AND Date_de_naissance='".$dn."' ";
+		// $sql = "SELECT * FROM Client WHERE Nom='".$name."' AND Prenom='".$surname."' AND Date_de_naissance='".$dn."' ";
+		$sql = "SELECT * FROM Client WHERE Nom='$name' AND Prenom='$surname' AND Date_de_naissance='$dn' ";
 
 		$res = mysqli_query($conn, $sql);
 
 		// Check login info
 		if(mysqli_num_rows($res) > 0) {
     		//echo "Login sucessfully !";
+    		session_start();
+
+    		while($row = mysqli_fetch_assoc($res)) {
+    			$_SESSION['name'] = $row["Nom"];
+    			$_SESSION['surname'] = $row["Prenom"];
+    			$_SESSION['dn'] = $row["Date_de_naissance"];
+        		$_SESSION['id'] = $row["ID_Client"];
+        		$_SESSION['ad'] = $row["Addresse"];
+        		$_SESSION['tel'] = $row["Numero_de_telephone"];
+    		}
+
     		header('Location: home_user.html');
 		} 
 		else{
-    		echo "ERROR: Your account is not found ! ";
+    		echo "ERROR: Your account is not found ! "."<br>";
 		}
 
 		// Statement 
