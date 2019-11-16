a8cf5d2b7885c3ccc44ded1ed0be90b54da19fad
Nu Huyen Trang Pham <nu-huyen-trang.pham@etu.utc.fr>
2019-11-16 16:31:27 +0100

    update

--- www/sign_up.php
+++ www/sign_up.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Hello World</title>
+	<title>Sign Up</title>
 </head>
 <body>
 	<?php  
@@ -10,25 +10,31 @@
 		$username = "root";
 		$password = "";
 		$dbname = "na17";
+
 		// Create connection
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
+
 		// Check connection
 		if (!$conn) {
    			die("Connection failed: " . $conn->connect_error);
 		}
+
 		// affection variable
 		$name = $_POST["name"];
 		$surname = $_POST["surname"];
 		$dn = $_POST["dn"];
 		$ad = $_POST["ad"];
 		$phone = $_POST["phone"];
+
 		//requete insert into table 
 		$sql = "INSERT INTO client (Nom, Prenom, Date_de_naissance, Addresse, Numero_de_telephone)
 		VALUES (?, ?, ?, ?, ?)";
+
 		//statement 
 		$stmt = $conn->prepare("$sql");
 		$stmt->bind_param("ssdss", $name, $surname, $dn, $ad, $phone);
 		$stmt->execute();
+
 		//close connection
 		$conn->close();
 	?>
