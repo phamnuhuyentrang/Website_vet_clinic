0845a51cdf59e338aec64b2406a5d97b51ddba63
Nu Huyen Trang Pham <nu-huyen-trang.pham@etu.utc.fr>
2019-11-19 13:11:53 +0100

    update

--- www/rdv.php
+++ www/rdv.php
@@ -20,6 +20,9 @@
 		$id = $_SESSION['id'];
 		$sql = "SELECT ID_Animal, Nom FROM Animal WHERE ID_Client = '$id' ";
 		$res = mysqli_query($conn, $sql);
+
+		$sql2 = "SELECT R.No_ref, A.Nom, V.Nom, V.prenom, R.Date_rdv, R.Time_rdv FROM Rendez_vous R LEFT JOIN Animal A ON R.Pet = A.ID_Animal LEFT JOIN Veterinaire V ON R.Vet = V.ID_personnel WHERE A.ID_Client='$id' ORDER BY R.Date_rdv AND R.Time_rdv";
+		$res2 = mysqli_query($conn, $sql2);
 	}
 	else {
 		echo "Session ends.";
@@ -56,15 +59,19 @@
 			<section id="banner">
 				<h2>President And Secretary Vet Clinic</h2>
 				<p>Your pet's health, we care</p>
+				<ul class="actions">
+					<li><a href="#rdv" class="button special big">New Appointment</a></li>
+					<li><a href="#rdv_view" class="button special big">Your Appointments</a></li>
+				</ul>
 			</section>
 
-		<!-- Profile -->
+		<!-- Add new appointment -->
 			<section id="rdv" class="wrapper style1">
 					<div class="container 75%">
 						<div class="row 200%">
 							<div class="6u 12u$(medium)">
 								<header class="major">
-									<h2>Your Appointment</h2>
+									<h2>New Appointment</h2>
 								</header>
 							</div>
 							<div class="6u$ 12u$(medium)">
@@ -94,5 +101,36 @@
 						</div>
 					</div>
 			</section>
+
+		<!--See all appointments-->
+			<section id="rdv_view" class="wrapper style2 special">
+					<div class="container">
+						<header class="major">
+							<h2>Your Appointments</h2>
+						</header>
+						<div class="row 150%">
+							<?php while($row1 = mysqli_fetch_array($res2)):;?>
+							<div class="6u 12u$(xsmall)">					
+								<div class="form">
+									<br>
+									<label for="ref">No: <?php echo $row1[0];?></label>
+								</div>
+								<div class="form">
+									<label for="pet">Pet: <?php echo $row1[1];?></label>
+								</div>
+								<div class="form">
+									<label for="vet">Vet: <?php echo $row1[2]. " ". $row1[3];?></label>
+								</div>
+								<div class="form">
+									<label for="date_rdv">Date: <?php echo $row1[4];?></label>
+								</div>
+								<div class="form">
+									<label for="time_rdv">Time: <?php echo $row1[5];?></label>
+								</div>
+							</div>
+							<?php endwhile;?>
+						</div>
+					</div>
+				</section>
 	</body>
 </html>
